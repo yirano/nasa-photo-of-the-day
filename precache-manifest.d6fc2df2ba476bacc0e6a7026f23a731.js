cb4230bc6f2371123e947c02a55ba8b3f66a423f
yirano <yirano@protonmail.ch>
2020-02-25 14:52:36 -0500

    Updates

---
+++ precache-manifest.d6fc2df2ba476bacc0e6a7026f23a731.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd0b95304e796205c6a975837c5d26a",
+    "url": "/nasa-photo-of-the-day/index.html"
+  },
+  {
+    "revision": "866f13b373b8d557b3cb",
+    "url": "/nasa-photo-of-the-day/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8be4ebfc7d1ec91aac2a",
+    "url": "/nasa-photo-of-the-day/static/js/2.af51ed4d.chunk.js"
+  },
+  {
+    "revision": "866f13b373b8d557b3cb",
+    "url": "/nasa-photo-of-the-day/static/js/main.a1db9bb9.chunk.js"
+  },
+  {
+    "revision": "350a0b8fbf7e20aef48a",
+    "url": "/nasa-photo-of-the-day/static/js/runtime~main.03586219.js"
+  }
+]);
