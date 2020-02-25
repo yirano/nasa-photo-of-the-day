06c1fcc298b6940acbdcf0b7a732b6090c046d16
yirano <yirano@protonmail.ch>
2020-02-25 14:54:13 -0500

    Remove redundant codes/Clean up codes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTitle from './Components/PageTitle/PageTitle';
 import MediaContainer from './Components/MediaContainer/MediaContainer';
 
--- src/Components/ImageContainer/ImageContainer.jsx
+++ src/Components/ImageContainer/ImageContainer.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Image from './Image';
 
 export default function ImageContainer(props) {
   const { image } = props;
-	return (
-		<div className="imageContainer">
-      <img src={image.url} alt={image.desc}/>
-		</div>
-	);
+  return (
+    <div className="imageContainer">
+      <img src={image.url} alt={image.desc} />
+    </div>
+  );
 }
